9a24f4dd14292b245745edf454719e1f702892cd
CloudWangASP <cloud_318@163.com>
2017-09-08 22:31:07 +0800

    logo页面的layout

--- QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
+++ QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.xc.qspousab;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+Button yuangong
+        Button boss
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+yuangong=(Button)
+
+    @Override
+    public View findViewById(@IdRes int id) {
+        return super.findViewById(id);
     }
 }
