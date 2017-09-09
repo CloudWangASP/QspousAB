e69bc0092b99a848b04b157484762ffe2fd63c87
CloudWangASP <cloud_318@163.com>
2017-09-09 22:24:44 +0800

    引入第三方库

--- QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
+++ QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.xc.qspousab;
 
 import android.content.Intent;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.canyinghao.candialog.CanDialog;
+import com.canyinghao.candialog.CanDialogInterface;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,9 +25,25 @@ public class LoginActivity extends AppCompatActivity {
         yuangong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(LoginActivity.this, Login2Activity.class);
-                startActivity(intent);
+                new CanDialog.Builder(LoginActivity.this)
+                        .setIconType(CanDialog.ICON_INFO)
+//                        .setTitle("Dialog Title")
+                        .setEditDialog("请在此输入密码", true, 8, 0)
+                        .setNegativeButton("取消", true, null)
+                        .setPositiveButton("确定", true, new CanDialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(CanDialog dialog, int position, CharSequence text, boolean[] checkitems) {
+
+
+//                                dialog.setAnimationMessage(CanDialog.ANIM_INFO_SUCCESS, "Password is " + text.toString());
+//                                dialog.setPositiveButton("sure", true, null);
+
+
+                            }
+                        })
+                        .setCircularRevealAnimator(CanDialog.CircularRevealStatus.BOTTOM_RIGHT)
+                        .setCancelable(true)
+                        .show();
             }
         });
         boss.setOnClickListener(new View.OnClickListener() {
