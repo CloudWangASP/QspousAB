96105da1f328e7b6e0e7bef2891400ef4bbb9e28
CloudWangASP <cloud_318@163.com>
2017-09-14 23:11:51 +0800

    引入第三方弹框

--- QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
+++ QspousAB/app/src/main/java/com/xc/qspousab/LoginActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.canyinghao.candialog.CanDialog;
-import com.canyinghao.candialog.CanDialogInterface;
+import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
+import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,24 +26,33 @@ public class LoginActivity extends AppCompatActivity {
         yuangong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new CanDialog.Builder(LoginActivity.this)
-                        .setIconType(CanDialog.ICON_INFO)
-//                        .setTitle("Dialog Title")
-                        .setEditDialog("请在此输入密码", true, 8, 0)
-                        .setNegativeButton("取消", true, null)
-                        .setPositiveButton("确定", true, new CanDialogInterface.OnClickListener() {
+                NiftyDialogBuilder dialogBuilder=NiftyDialogBuilder.getInstance(LoginActivity.this);
+                dialogBuilder
+                        .withTitle("Modal Dialog")                                  //.withTitle(null)  no title
+                        .withTitleColor("#FFFFFF")                                  //def
+                        .withDividerColor("#11000000")                              //def
+                        .withMessage("This is a modal Dialog.")                     //.withMessage(null)  no Msg
+                        .withMessageColor("#FFFFFFFF")                              //def  | withMessageColor(int resid)
+                        .withDialogColor("#FFE74C3C")                               //def  | withDialogColor(int resid)
+                        .withIcon(getResources().getDrawable(R.drawable.logo))
+                        .withDuration(700)                                          //def
+                        .withEffect(Effectstype.Slidetop)                                         //def Effectstype.Slidetop
+                        .withButton1Text("OK")                                      //def gone
+                        .withButton2Text("Cancel")                                  //def gone
+                        .isCancelableOnTouchOutside(true)                           //def    | isCancelable(true)
+                        .setCustomView(R.layout.dialog_layout,v.getContext())         //.setCustomView(View or ResId,context)
+                        .setButton1Click(new View.OnClickListener() {
                             @Override
-                            public void onClick(CanDialog dialog, int position, CharSequence text, boolean[] checkitems) {
-
-
-//                                dialog.setAnimationMessage(CanDialog.ANIM_INFO_SUCCESS, "Password is " + text.toString());
-//                                dialog.setPositiveButton("sure", true, null);
-
-
+                            public void onClick(View v) {
+                                Toast.makeText(v.getContext(), "i'm btn1", Toast.LENGTH_SHORT).show();
                             }
                         })
-                        .setCircularRevealAnimator(CanDialog.CircularRevealStatus.BOTTOM_RIGHT)
-                        .setCancelable(true)
+                        .setButton2Click(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(v.getContext(),"i'm btn2",Toast.LENGTH_SHORT).show();
+                            }
+                        })
                         .show();
             }
         });
