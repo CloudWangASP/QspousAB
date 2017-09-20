e5e7fc28020bb6051aacabe27a6c6b5b0ecea077
CloudWangASP <cloud_318@163.com>
2017-09-20 23:04:54 +0800

    代码优化：解决竖线显示问题，完成输入页面ui

--- QspousAB/app/src/main/java/com/xc/qspousab/adapter/DetailAdapter.java
+++ QspousAB/app/src/main/java/com/xc/qspousab/adapter/DetailAdapter.java
@@ -17,6 +17,8 @@ public class DetailAdapter extends BaseAdapter {
 
     private TextView incomeOutput;
     private TextView payOutput;
+    private View rightLine;
+    private View leftLine;
     LayoutInflater layoutInflater;
 
     public DetailAdapter(Context context) {
@@ -40,12 +42,24 @@ public class DetailAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        convertView = layoutInflater.inflate(R.layout.adapter_detail_item_view, null);
+        convertView = layoutInflater.inflate(R.layout.adapter_detail_view, null);
         incomeOutput = (TextView) convertView.findViewById(R.id.incomeOutput);
         payOutput = (TextView) convertView.findViewById(R.id.payOutput);
-
+        rightLine = convertView.findViewById(R.id.rightLine);
+        leftLine = convertView.findViewById(R.id.leftLine);
         incomeOutput.setText("+450");
         payOutput.setText("-130");
+
+        /**
+         * 模拟真实数据
+         **/
+        if (position % 3 == 0) {
+            incomeOutput.setVisibility(View.INVISIBLE);
+            leftLine.setVisibility(View.INVISIBLE);
+        } else {
+            payOutput.setVisibility(View.INVISIBLE);
+            rightLine.setVisibility(View.INVISIBLE);
+        }
 
         return convertView;
     }
